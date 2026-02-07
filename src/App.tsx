f101172ddbf6f125d845909e30305339b6538aad
oluochvn <red.oluoch@gmail.com.com>
2026-02-07 03:20:57 +0300

    new2

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,28 @@ import { useState } from "react";
 import { Sun } from "lucide-react";
 import { Moon } from "lucide-react";
 import { MenuIcon } from "lucide-react";
+import { X } from "lucide-react";
 
 function App(){
           const [dark,setDark] = useState(false)
           const [open, setOpen] = useState(false);
 
   return(
- <div className={`${dark ? "bg-black" : "bg-white"} min-h-screen`}>
+ <div className={`${dark ? "bg-black" : "bg-white"} ${dark ? "text-white" : "text-black"} min-h-screen`}>
   <div className={`${dark ? "bg-red-500" : "bg-green-400"}`}>
     <nav className="flex items-center justify-between p-4 md:p-5 md:justify-around">
 
-      {/* logo */}
       <h1 className="font-bold">VIN</h1>
 
-      {/* menu links */}
       <ul className={`
         ${open ? "flex" : "hidden"} 
-        flex-col absolute top-16 left-0 w-[20%] bg-white p-5 gap-4
-        md:static md:flex md:flex-row md:bg-transparent md:w-auto md:p-0
+        flex-col absolute top-16 left-0 w-[20%]  p-5 gap-4
+        md:static md:flex md:flex-row md:bg-transparent md:w-auto  md:p-0
       `}>
         <li>Home</li>
-        <li>Home</li>
-        <li>Home</li>
-        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact me</li>
       </ul>
 
       <div className="flex items-center gap-6">
@@ -44,7 +43,8 @@ function App(){
         </button>
 
         <button className="md:hidden" onClick={() => setOpen(!open)}>
-          <MenuIcon />
+          <MenuIcon className={`absolute ${open ? "opacity-0" : "opacity-100"} `} />
+          <X className={` ${open ? "opacity-100" : "opacity-0"} `}  />
         </button>
 
       </div>
