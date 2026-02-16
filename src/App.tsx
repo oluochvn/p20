1942cb783e119b4f15848877c514158527302d98
oluochvn <red.oluoch@gmail.com>
2026-02-17 00:55:23 +0300

    done112

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,18 @@ function App(){
 
   return(
  <div className={`${dark ? "bg-black" : "bg-white"} ${dark ? "text-white" : "text-black"} transition duration-500 ease-out min-h-screen font-[Roboto]`}>
+   
    <div className="fixed inset-0 w-full h-full pointer-events-none z-40">
      <Snowfall color="#34374C" />
    </div>
 
-   <nav className={`${dark ? "bg-neutral-900" : "bg-neutral-300"} flex items-center justify-between p-4 md:p-5 md:justify-around sticky top-0 z-50`}>
+   <nav className={`${dark ? "bg-neutral-900/80" : "bg-neutral-300/80"} backdrop-blur-md flex items-center justify-between p-4 md:p-5 md:justify-around sticky top-0 z-50`}>
       <h1 className="font-bold">Oluochvn</h1>
 
       <ul className={`
         ${open ? "flex" : "hidden"} 
-        flex-col absolute top-16 left-0 w-full text-center p-6 gap-6 text-lg
-        ${dark ? "bg-neutral-900/90" : "bg-neutral-200/90"}
+        flex-col absolute top-16 left-0 w-full text-center p-8 gap-8 text-lg
+        ${dark ? "bg-neutral-900/95" : "bg-neutral-200/95"}
         backdrop-blur-md
         md:static md:flex md:flex-row md:bg-transparent md:w-auto md:p-0 md:text-base md:gap-4
       `}>
@@ -44,13 +45,22 @@ function App(){
       </div>
     </nav>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 min-h-[20vh] px-6 pt-16 md:pt-0 md:min-h-screen md:pb-45">
-      <div className="text-center md:text-left md:mt-[30%] md:ml-[15%] text-3xl md:text-4xl font-mono font-bold space-y-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen px-6 pt-28 md:pt-0 md:min-h-screen md:pb-45 relative overflow-hidden">
+
+      <div className="absolute inset-0 flex justify-center md:hidden">
+        <div className="w-[320px] h-[320px] bg-yellow-600/20 rounded-full blur-3xl"></div>
+      </div>
+
+      <div className="relative text-center md:text-left flex flex-col justify-center md:mt-[30%] md:ml-[15%] text-3xl md:text-4xl font-mono font-bold space-y-6">
         <h1>Vincent Oluoch</h1>
         <div className="space-y-3">
           <h1 className="text-yellow-600">FullStack Developer</h1>
-          <p className="text-base md:text-[0.7em] md:mt-2">I make modern websites that are fast and simple to use.</p>
-          <button className="mx-auto md:mx-0"><img src="/github.svg" alt="" /></button>
+          <p className="text-base md:text-[0.7em] md:mt-2 max-w-sm mx-auto md:mx-0">
+            I make modern websites that are fast and simple to use.
+          </p>
+          <button className="mx-auto md:mx-0">
+            <img src="/github.svg" alt="" />
+          </button>
         </div>
       </div>
 
@@ -59,10 +69,10 @@ function App(){
       </div>
     </div>
 
-    <div id="about" className={`px-6 py-12 md:grid md:grid-cols-2 md:min-h-screen ${dark ? "bg-neutral-900" : "bg-[#e3e4d2]"}`}>
-      <div className="space-y-6 flex flex-col justify-center md:items-center">
-        <h1 className="text-4xl md:text-6xl font-[saira] text-center md:text-left">ABOUT</h1>
-        <p className="text-lg md:text-[1.4em] max-w-2xl text-center md:text-left">
+    <div id="about" className={`px-6 py-20 md:grid md:grid-cols-2 md:min-h-screen ${dark ? "bg-neutral-900" : "bg-[#e3e4d2]"}`}>
+      <div className="space-y-6 flex flex-col justify-center md:items-center text-center md:text-left">
+        <h1 className="text-4xl md:text-6xl font-[saira]">ABOUT</h1>
+        <p className="text-lg md:text-[1.4em] max-w-2xl mx-auto md:mx-0">
           I’m a Full-Stack Developer who builds Fast, Responsive, and practical web applications. I enjoy creating clean user experiences and writing efficient, maintainable code on both the Frontend and Backend. I’m always learning, improving through real projects, and focused on building simple solutions that solve real problems.
         </p>
       </div>
@@ -74,7 +84,7 @@ function App(){
       </div>
     </div>
 
-    <div id="projects" className={`min-h-screen ${dark ? "bg-gradient-to-b from-neutral-950 to-neutral-900 text-white" : "bg-[#d7d8c5] text-black"} pb-16 px-4`}>
+    <div id="projects" className={`min-h-screen ${dark ? "bg-gradient-to-b from-neutral-950 to-neutral-900 text-white" : "bg-[#d7d8c5] text-black"} pb-20 px-4`}>
       <div className="text-center pt-20 space-y-4">
         <h1 className="text-4xl md:text-5xl font-[saira] tracking-wide">PROJECTS</h1>
         <p className={`${dark ? "text-white/50" : "text-black/60"} max-w-xl mx-auto text-sm md:text-base`}>
@@ -82,9 +92,9 @@ function App(){
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 max-w-6xl mx-auto gap-8 md:gap-10 mt-10 md:mt-14">
-        <div className="group relative rounded-3xl overflow-hidden border border-white/10 backdrop-blur-xl bg-white/5 hover:-translate-y-2 transition duration-500">
-          <img className="w-full h-[25vh] md:h-[45vh] object-cover group-hover:scale-105 transition duration-500" src="/website.png" alt="" />
+      <div className="grid grid-cols-1 md:grid-cols-2 max-w-6xl mx-auto gap-8 md:gap-10 mt-14">
+        <div className="group relative rounded-3xl overflow-hidden border border-white/10 backdrop-blur-xl bg-white/5 hover:-translate-y-2 transition duration-500 p-1">
+          <img className="w-full h-[30vh] md:h-[45vh] object-cover rounded-2xl group-hover:scale-105 transition duration-500" src="/website.png" alt="" />
           <div className="p-5 md:p-6 space-y-3">
             <h3 className="text-xl md:text-2xl font-semibold">Portfolio Website</h3>
             <div className="flex gap-2 flex-wrap">
@@ -95,8 +105,8 @@ function App(){
           </div>
         </div>
 
-        <div className="group relative rounded-3xl overflow-hidden border border-white/10 backdrop-blur-xl bg-white/5 hover:-translate-y-2 transition duration-500">
-          <img className="w-full h-[25vh] md:h-[45vh] object-cover group-hover:scale-105 transition duration-500" src="/paytrack.png" alt="" />
+        <div className="group relative rounded-3xl overflow-hidden border border-white/10 backdrop-blur-xl bg-white/5 hover:-translate-y-2 transition duration-500 p-1">
+          <img className="w-full h-[30vh] md:h-[45vh] object-cover rounded-2xl group-hover:scale-105 transition duration-500" src="/paytrack.png" alt="" />
           <div className="p-5 md:p-6 space-y-3">
             <h3 className="text-xl md:text-2xl font-semibold">PayTrack</h3>
             <div className="flex gap-2 flex-wrap">
@@ -108,25 +118,28 @@ function App(){
         </div>
       </div>
 
-      <div className="flex justify-center mt-12 md:mt-14">
-        <button className="bg-amber-400 px-6 md:px-8 py-3 rounded-full font-semibold text-black hover:scale-105 transition">
+      <div className="flex justify-center mt-14">
+        <button className="bg-amber-400 px-8 py-3 rounded-full font-semibold text-black hover:scale-105 transition">
           View More
         </button>
       </div>
     </div>
 
     <section id="contact" className="min-h-screen flex items-center justify-center px-4 py-20">
-      <div className="max-w-3xl w-full rounded-2xl p-6 md:p-8 shadow-lg space-y-6">
+      <div className={`max-w-3xl w-full rounded-3xl p-8 shadow-lg space-y-6 
+      ${dark 
+        ? "bg-gradient-to-br from-neutral-900 to-black border border-white/10" 
+        : "bg-white border border-black/10"}`}>
         <h2 className="text-2xl md:text-3xl font-bold text-center">Contact Me</h2>
         <p className="text-center text-sm md:text-base">
           Got a project, idea, or just want to say hi? Send me a message
         </p>
 
         <form className="space-y-4">
-          <input type="text" placeholder="Your Name" className="w-full p-3 rounded-xl border outline-none focus:ring-2 focus:ring-white/20"/>
-          <input type="email" placeholder="Your Email" className="w-full p-3 rounded-xl border outline-none focus:ring-2 focus:ring-white/20"/>
-          <textarea placeholder="Your Message" className={`w-full p-3 rounded-xl ${dark ? "text-white" : "text-black"} border outline-none focus:ring-2 ${dark ? "focus:ring-white" : "focus:ring-white/20"}`}></textarea>
-          <button type="submit" className={`w-full ${dark ? "bg-amber-600" : "bg-amber-300"} font-semibold p-3 rounded-xl hover:opacity-80 transition`}>
+          <input type="text" placeholder="Your Name" className="w-full p-3 rounded-xl border bg-transparent outline-none focus:ring-2 focus:ring-white/20"/>
+          <input type="email" placeholder="Your Email" className="w-full p-3 rounded-xl border bg-transparent outline-none focus:ring-2 focus:ring-white/20"/>
+          <textarea placeholder="Your Message" className="w-full p-3 rounded-xl border bg-transparent outline-none focus:ring-2 focus:ring-white/20"></textarea>
+          <button type="submit" className="w-full bg-amber-600 font-semibold p-3 rounded-xl hover:opacity-80 transition">
             Send Message
           </button>
         </form>
@@ -136,4 +149,5 @@ function App(){
  </div>
   )
 }
+
 export default App;
