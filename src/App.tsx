d6d5afa4b0a31556ebd001bb0823bd859814917a
vnn@dell <red.oluoch@gmail.com>
2026-07-16 17:44:56 +0300

    nw

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App(){
   
   return (
     <div className={`${darkMode ? 'bg-gray-950 text-white' : 'bg-neutral-200 text-gray-800'} h-screen  w-full h-[300px] overflow-hidden transition-colors duration-700 font-jetbrains-mono`}>
-    <nav className=" flex justify-between items-center p-4 border-b border-gray-300 ml-5 mr-5">
+    <nav className=" flex justify-between items-center p-4 ml-5 mr-5 h-[100px]">
       <div>
         <h1 className="font-caveat text-2xl">Vincent Oluoch</h1>
       </div>
@@ -24,11 +24,11 @@ function App(){
           <li><a href="#Contact" className="hover:text-blue-500">Contact</a></li>
         </ul>
       </div>
-      <div>
+      <div className="flex items-center">
          <button className="mr-3 transition-transform duration-1700" onClick={() => setDarkMode(!darkMode)}>
         {darkMode ? <Sun/> : <Moon  />}
       </button>
-      </div>
+      
       <div className="flex">
        <button className=" lg:hidden transition-all duration-800"
   onClick={() => setOpen(!open)}
@@ -37,13 +37,14 @@ function App(){
 </button>
          
       </div>
+      </div>
     </nav>
-    <div className={`lg:hidden ${open ? 'block' : 'hidden'} p-4 text-center transition-transform duration-700`}>
-      <ul className="space-y-2">
-        <li><a href="#home" className="hover:text-blue-500">Home</a></li>
-        <li><a href="#About" className="hover:text-blue-500">About</a></li>
-        <li><a href="#Projects" className="hover:text-blue-500">Projects</a></li>
-        <li><a href="#Contact" className="hover:text-blue-500">Contact</a></li>
+    <div className={`lg:hidden ${open ? 'block' : 'hidden'} p-4 text-center transition-transform duration-700 bg-gray-600 w-full text-white`}>
+      <ul className="space-y-2  ">
+        <li><a href="#home" className="hover:text-blue-900 pb-7">Home</a></li>
+        <li><a href="#About" className="hover:text-blue-900 pb-7">About</a></li>
+        <li><a href="#Projects" className="hover:text-blue-900 pb-7">Projects</a></li>
+        <li><a href="#Contact" className="hover:text-blue-900 pb-7">Contact</a></li>
       </ul>
     </div>
   </div>
