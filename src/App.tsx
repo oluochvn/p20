a5d3bf89439a038ac29e4739dd923d0743b54965
vnn@dell <red.oluoch@gmail.com>
2026-07-16 12:38:26 +0300

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@
 function App(){
 
   return (
-    <div>
-    <nav className=" flex justify-between items-center p-4 bg-gray-800 text-white">
+    <div className="font-jetbrains-mono">
+    <nav className=" flex justify-between items-center p-4 border-b border-gray-300 ml-5 mr-5">
       <div>
-        <h1>Vincent Oluoch</h1>
+        <h1 className="font-caveat text-2xl">Vincent Oluoch</h1>
       </div>
       <div>
         <ul className="flex space-x-5">
@@ -15,7 +15,7 @@ function App(){
           <li>Contact</li>
         </ul>
       </div>
-      <div className="m-2">dark</div>
+      <div className="mr-3">dark</div>
     </nav>
     </div>
   )
