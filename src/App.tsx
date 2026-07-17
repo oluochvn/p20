fef404d67d96acbc92e21686e91fcaa4d05e2516
vnn@dell <red.oluoch@gmail.com>
2026-07-17 23:01:47 +0300

    mmdsa

--- src/App.tsx
+++ src/App.tsx
@@ -24,10 +24,10 @@ function App(){
       </div>
       <div className=" lg:flex hidden space-x-5 ">
         <ul className="lg:flex hidden space-x-5">
-          <li><a href="#home" className="hover:text-blue-500">Home</a></li>
-          <li><a href="#About" className="hover:text-blue-500">About</a></li>
-          <li><a href="#Projects" className="hover:text-blue-500">Projects</a></li>
-          <li><a href="#Contact" className="hover:text-blue-500">Contact</a></li>
+          <li><a href="#home" className="hover:text-green-600">Home</a></li>
+          <li><a href="#About" className="hover:text-green-600">About</a></li>
+          <li><a href="#Projects" className="hover:text-green-600">Projects</a></li>
+          <li><a href="#Contact" className="hover:text-green-600">Contact</a></li>
         </ul>
       </div>
       <div className="flex items-center">
@@ -54,14 +54,14 @@ function App(){
       </ul>
     </div>
 
-    <div className="lg:pl-15 lg:pr15 grid grid-cols-2  ml-15 mr-15 mt-20 text-align-center justify-items-center">
+    <div className="lg:pl-15 lg:pr15 grid grid-cols-2  ml-15 mr-15 mt-20 text-align-center justify-items-center h-[320px]">
   <div className="">
     <h4 className="text-2xl ml-2">HI, I'M</h4>
-    <h1 className="text-7xl font-bold">
-      VINCENT <br />
-      OLUOCH
+    <h1 className="text-5xl font-bold">
+      VINCENT OLUOCH
     </h1>
-    <h4 className="text-green-600 font-bold text-2xl pt-5 ">Fullstack Developer</h4> 
+    <h4 className="text-green-600 font-bold text-2xl  ">Fullstack Developer</h4> 
+    <p className="pt-4">I solve design problems, create intuitive user interfaces, and craft engaging web experiences and modern web applications.</p>
     
      <div className="flex items-center space-x-2 mt-4 font-bold hover:text-green-600 "><MapPin /><h2>KENYA</h2></div>
   </div>
@@ -76,16 +76,16 @@ function App(){
 </div>
 
       <div className="block m-10 h-[300px]">
-      <div>
-        <h2>About</h2>
-      </div>
+      <div className="ml-17 mr-15">
+        <h1 className="text-4xl font-bold">Skills</h1>
+      </div> 
       <div></div>
     </div>
 
       <div className="block m-10 h-[300px]">
-      <div>
-        <h2>About</h2>
-      </div>
+      <div className="ml-17 mr-15">
+        <h1 className="text-4xl font-bold">About</h1>
+      </div> 
       <div></div>
     </div>
   </div>
